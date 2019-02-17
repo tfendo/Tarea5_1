ed6c77f7692ab9dc79777813917499c9d8ea11c8
Hugo Guillín <hugoguillin@hotmail.com>
2019-02-17 11:20:23 +0100

    Task_4

--- app/src/main/java/com/hugoguillin/scorekeeper/MainActivity.java
+++ app/src/main/java/com/hugoguillin/scorekeeper/MainActivity.java
@@ -2,6 +2,9 @@ package com.hugoguillin.scorekeeper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -9,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
 
     private int mScore1, mScore2;
     private TextView mScoreText1, mScoreText2;
+    static final String STATE_SCORE_1 = "Team 1 Score";
+    static  final String STATE_SCORE_2 = "Team 2 Score";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mScoreText1 = findViewById(R.id.score_1);
         mScoreText2 = findViewById(R.id.score_2);
+        if(savedInstanceState != null){
+            mScore1 = savedInstanceState.getInt(STATE_SCORE_1);
+            mScore2 = savedInstanceState.getInt(STATE_SCORE_2);
+
+            mScoreText1.setText(Integer.toString(mScore1));
+            mScoreText2.setText(Integer.toString(mScore2));
+        }
 
     }
 
@@ -47,5 +59,38 @@ public class MainActivity extends AppCompatActivity {
                     mScoreText2.setText(Integer.toString(mScore2));
 
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+        if(nightMode == AppCompatDelegate.MODE_NIGHT_YES){
+            menu.findItem(R.id.night_mode).setTitle(R.string.day_mode);
+        }else{
+            menu.findItem(R.id.night_mode).setTitle(R.string.night_mode);
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()== R.id.night_mode){
+            int nightMode = AppCompatDelegate.getDefaultNightMode();
+            if (nightMode == AppCompatDelegate.MODE_NIGHT_YES){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }
+            recreate();
+        }
+        return true;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(STATE_SCORE_1, mScore1);
+        outState.putInt(STATE_SCORE_2, mScore2);
+        super.onSaveInstanceState(outState);
     }
 }
